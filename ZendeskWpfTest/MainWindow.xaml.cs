f13bb51169a0e02dd83820506fad7473bea0e320
pmaltsev <pmaltsev@malkosua.com>
2014-01-28 16:50:00 -0500

    small WPF test added

---
+++ ZendeskWpfTest/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ZendeskApi_v2.Models.Constants;
+using ZendeskApi_v2.Models.Tickets;
+using ZendeskApi_v2;
+
+
+namespace ZendeskWpfTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //ZendeskApi api = new ZendeskApi(https://surfer123.zendesk.com/api/v2, "surfer123@yandex.ru", "");
+            var apiToken = new ZendeskApi(APIURL.Text, "surfer123@yandex.ru", ""); 
+
+            var ticket = apiToken.Tickets.CreateTicket(new Ticket()
+            {
+                Subject = "my printer is on fire",
+                Comment = new Comment() { Body = "HELP" },
+                Priority = TicketPriorities.Urgent
+            });
+        }
+    }
+}
